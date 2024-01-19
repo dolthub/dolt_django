66dc4d012f05e8351324beb49e8b0cdd54a0435b
Tim Sehn <tim@dolthub.com>
2024-01-19 13:54:41 -0800

    Stubbed out question detail page

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Question
-
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -12,8 +11,8 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
