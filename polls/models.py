96227b1918237b230506c4d5d2d0bb3d750b65c3
Tim Sehn <tim@dolthub.com>
2024-01-19 14:49:47 -0800

    Added tests

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -8,7 +11,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
