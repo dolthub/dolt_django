e090236b3ba141e3b348dba54465200fa73918cd
Tim Sehn <tim@dolthub.com>
2024-01-19 16:37:34 -0800

    Working Dolt commits

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,12 @@
 import datetime
 
-from django.db import models
+from django.db import models, connection
 from django.utils import timezone
 from django.contrib import admin
+
+def _dolt_commit(message):
+    with connection.cursor() as cursor:
+        cursor.execute("CALL DOLT_COMMIT('-Am', '" + message + "')")
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -19,6 +23,11 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def save(self, *args, **kwargs):
+        super(Question, self).save(*args, **kwargs)
+
+        _dolt_commit("Saved a question")        
     
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -27,3 +36,9 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+    def save(self, *args, **kwargs):
+        super(Choice, self).save(*args, **kwargs)
+
+        _dolt_commit("Saved a choice")
+
